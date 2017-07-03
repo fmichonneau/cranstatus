2a08a4778fababc412a9e240135aa4c857ab9a03
Francois Michonneau <francois.michonneau@gmail.com>
2017-07-03 17:39:23 -0400

    better download logic

--- api.R
+++ api.R
@@ -1,7 +1,25 @@
 library(foghorn)
 library(dplyr)
 
-crandb <- foghorn:::get_cran_rds_file("results")
+
+download_cran_file <- function(file = "results") {
+    if (!dir.exists("data")) {
+        dir.create("data")
+    }
+    latest <- file.path("data", paste0("latest_check_", file, ".rds"))
+    timestamp <- format(Sys.time(), "%Y%m%d-%H%M%S")
+    crandb <- foghorn:::fetch_cran_rds_file(file,
+                                            dest = "data",
+                                            file_prefix = paste0(timestamp, "_"))
+    if (file.exists(crandb)) {
+        if (file.exists(latest)) file.remove(latest)
+        file.symlink(normalizePath(crandb),
+                     normalizePath(latest, mustWork = FALSE))
+    }
+    check_cran_rds_file(latest, return_logical = TRUE)
+}
+download_cran_file()
+
 
 #* @get /package_status
 package_status <- function(package, email, ...) {
